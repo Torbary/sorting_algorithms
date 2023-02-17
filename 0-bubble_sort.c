4ac0e4cd5910898f9f31469c8c03a6e90aad098b
Torbary <torbarey020@gmail.com>
2023-02-17 01:39:22 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+
+/**
+ * swap - swap two integers
+ * @a: fist integer
+ * @b: second integer
+ *
+ * Return: 0
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sort an array of int in ascending order
+ * @array: pointer to the unsorted array
+ * @size: sise of the array
+ *
+ * Return: 0
+ */
+
+
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
