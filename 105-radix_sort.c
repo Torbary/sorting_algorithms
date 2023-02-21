ac589db98d4c42bda7f70aecf24a30d736c27c10
Torbary <torbarey020@gmail.com>
2023-02-20 23:02:21 -0800

    modified

---
+++ 105-radix_sort.c
@@ -0,0 +1,76 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sort an array of integers in ascending order using 
+ * counting sort algorithm
+ *
+ * @array: pointer to array to be sorted
+ * @size: size of the array
+ * @exponent: the current exponent
+ *
+ * Return: 0
+ */
+void counting_sort(int *array, int size, int exponent)
+{
+	int *output, *count;
+	int i
+
+	output = malloc(sizeof(int) * size);
+	count = malloc(sizeof(int) * 10);
+
+	for (i = 0; i < 10; i++)
+	{
+		count[i] = 0;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		count[(array[i] / exponent) % 10]++;
+	}
+
+	for (i = 0; i < 10; i++)
+	{
+		count[i] += count[i - 1];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exponent) % 10] - 1] = array[i];
+		count[(array[i] / exponent) % 10]--;
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = output[i];
+	}
+	print_array(array, size);
+	free(output);
+	free(count);
+}
+
+/**
+ * radix_sort - Sorts an array of integers in ascending order
+ * using the LSD radix sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: 0
+ */
+void radix_sort(int *array, size_t size)
+{
+	int i, max;
+	int exponent;
+
+	if (array == NULL | size < 2)
+		return;
+	max = array[0];
+
+	for (i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	for (exponent = 1; max / exponent > 0; exponent *= 10)
+	{
+		counting_sort(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -36,5 +36,7 @@ void top_down_split_merge(int *array, size_t start, size_t end, int *temp_array)
 void top_down_merge(int *array, size_t start, size_t mid, size_t end, int *temp_array);
 void sift_down(int *array, int start, int end);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, int size);
 void merge_sort(int *array, size_t size);
 #endif
