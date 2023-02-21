05dca2d12455771d9f8eccfcb33a15e60de3c8f3
Torbary <torbarey020@gmail.com>
2023-02-20 23:51:46 -0800

    header file modified

--- sort.h
+++ sort.h
@@ -30,13 +30,12 @@ void swap(int *a, int *b);
 void quicksort_recursive(int *array, int low, int high, size_t size);
 void cocktail_sort_list(listint_t **list);
 int partition(int *array, int low, int high, size_t size);
-void counting_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void top_down_split_merge(int *array, size_t start, size_t end, int *temp_array);
 void top_down_merge(int *array, size_t start, size_t mid, size_t end, int *temp_array);
 void sift_down(int *array, int start, int end);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
-void counting_sort(int *array, int size);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 #endif
