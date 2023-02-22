6ffb2c00e57a238df871bda43015751121f5dc51
Oladunsi <okeoladunsi6@gmail.com>
2023-02-21 19:54:33 -0800

    modified the sort.h file and fixing the errors

--- sort.h
+++ sort.h
@@ -10,16 +10,16 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 void insertion_sort_list(listint_t **list);
-void swap(int* xp, int* yp);
+void swap(int *xp, int *yp);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
